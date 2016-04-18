f3a642dae431ace23a446465b75ef619109bc181
Neil Hoang <ottohoang@gmail.com>
2016-04-18 08:20:24 -0500

    Revert "initial setup!"
    This reverts commit 46b9ee79b6061d075a3b30c56db6508d66a41cc4.

--- form-handler.js
+++
@@ -1,7 +0,0 @@
-function bus() {
-	document.getElementById("Addto").innerHTML = "Other html needed, e.g. <input type=text name=xxexample>";
-}
-function res(){
-	document.getElementById("Addto").innerHTML = "The html needed, e.g. <input type=text name=example>";
-
-}
