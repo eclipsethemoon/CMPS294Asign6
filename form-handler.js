46b9ee79b6061d075a3b30c56db6508d66a41cc4
Neil Hoang <ottohoang@gmail.com>
2016-04-18 08:17:02 -0500

    initial setup!
    Basic form setup

---
+++ form-handler.js
@@ -0,0 +1,7 @@
+function bus() {
+	document.getElementById("Addto").innerHTML = "Other html needed, e.g. <input type=text name=xxexample>";
+}
+function res(){
+	document.getElementById("Addto").innerHTML = "The html needed, e.g. <input type=text name=example>";
+
+}
